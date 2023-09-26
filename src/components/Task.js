e8f0d9b22d224b4d687229afac3d866c552e8f96
ArtemMartynow <artem1435@i.ua>
2023-09-26 23:52:41 +0300

    added icon

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { AiOutlineClose } from "react-icons/ai"
 
 
 class Task extends React.Component {
   render() {
     return (
       <div className='task'>
+        <AiOutlineClose className='icon' />
         <h1>{this.props.task.title}</h1>
       </div>
     )
