9c364d9e5d1eb00568e4fa6b7542e5eba27bcb7b
ArtemMartynow <artem1435@i.ua>
2023-10-03 03:59:44 +0300

    added a task completion mark

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,13 +3,28 @@ import { AiOutlineClose } from "react-icons/ai"
 
 
 class Task extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        done: 'task'
+    }
+    this.doneTask = this.doneTask.bind(this)
+}
   render() {
     return (
-      <div className='task'>
+      <div className={this.state.done} onClick={this.doneTask}>
         <AiOutlineClose className='icon' onClick={() => this.props.deleteTask(this.props.task.id)} />
         <h1>{this.props.task.title}</h1>
       </div>
     )
+  }
+
+  doneTask() {
+    if(this.state.done === 'task') {
+      this.setState({ done: 'done' })
+    } else {
+      this.setState({ done: 'task' })
+    }
   }
 }
 
